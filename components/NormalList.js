c96fc5ce3c3b4d9b4bb9a96fc941e1dd4339f35f
OrangeXC <842012622@qq.com>
2018-03-22 16:04:26 +0800

    Fixed normal list and timeline card style

--- components/NormalList.js
+++ components/NormalList.js
@@ -9,34 +9,47 @@ export default function NormalList(props) {
   )
 
   return (
-    <List
-      itemLayout="vertical"
-      size="large"
-      bordered
-      dataSource={props.list}
-      renderItem={item => (
-        <List.Item
-          key={item._id}
-          actions={[
-            <IconText type="user" text={ item.who ? item.who : '未知' } />,
-            <IconText type="clock-circle-o" text={ item.publishedAt.slice(0, 10) } />,
-            <IconText type="tag-o" text={item.type} />
-          ]}
-          extra={item.images &&
-            <div style={{ width: 272, height: 168, textAlign: 'center', lineHeight: '168px', backgroundColor: 'rgb(234, 237, 242)' }}>
-              <img
-                style={{ maxWidth: '100%', maxHeight: '100%' }}
-                alt="logo"
-                src={`${item.images[0].replace(/^http:\/\//i, 'https://')}?imageView2/0/w/544`}
-              />
-            </div>
-          }
-        >
-          <List.Item.Meta
-            title={<a href={item.url} target="_blank">{item.desc}</a>}
-          />
-        </List.Item>
-      )}
-    />
+    <div>
+      <List
+        itemLayout="vertical"
+        size="large"
+        bordered
+        dataSource={props.list}
+        renderItem={item => (
+          <List.Item
+            key={item._id}
+            actions={[
+              <IconText type="user" text={ item.who ? item.who : '未知' } />,
+              <IconText type="clock-circle-o" text={ item.publishedAt.slice(0, 10) } />,
+              <IconText type="tag-o" text={item.type} />
+            ]}
+            extra={item.images &&
+              <div style={{ width: 272, height: 168, textAlign: 'center', lineHeight: '168px', backgroundColor: 'rgb(234, 237, 242)' }}>
+                <img
+                  style={{ maxWidth: '100%', maxHeight: '100%' }}
+                  alt="cover"
+                  src={`${item.images[0].replace(/^http:\/\//i, 'https://')}?imageView2/0/w/544`}
+                />
+              </div>
+            }
+          >
+            <List.Item.Meta
+              title={<a href={item.url} target="_blank">{item.desc}</a>}
+            />
+          </List.Item>
+        )}
+      />
+      <style global jsx>{`
+        .ant-list-vertical .ant-list-item-main {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+
+        .ant-list-vertical .ant-list-item-action {
+          margin-left: 0;
+        }
+    `}</style>
+    </div>
   )
 }
