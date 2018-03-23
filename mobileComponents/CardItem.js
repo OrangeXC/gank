c39528c5f54f55d23222b772a0eb66024aa86ec5
OrangeXC <842012622@qq.com>
2018-03-23 11:09:34 +0800

    Add moblie error handler

--- mobileComponents/CardItem.js
+++ mobileComponents/CardItem.js
@@ -4,22 +4,47 @@ const CardItem = (rowData) => {
   return (
     <div>
       {
-        rowData.type !== '福利'
-          ? <Card full onClick={() => {window.open(rowData.url)}}>
-              <Card.Body>
-                <Flex align="start">
-                  {rowData.images && <Flex.Item style={{ flex: '0 0 auto', width: 100, textAlign: 'center' }}><img style={{ maxWidth: 100, maxHeight: 100 }} src={`${rowData.images[0].replace(/^http:\/\//i, 'https://')}?imageView2/0/w/200/h/200`} /></Flex.Item>}
-                  <Flex.Item><div>{rowData.desc}</div></Flex.Item>
-                </Flex>
-              </Card.Body>
-              <Card.Footer content={<div>{rowData.who ? rowData.who : ''} {rowData.publishedAt.slice(0, 10)}</div>} extra={<Tag small>{rowData.type}</Tag>} />
-            </Card>
-          : <Card full>
-              <Card.Body>
-                <img style={{ maxWidth: '100%' }} src={`${rowData.url}?imageView2/2/w/750`} />
-              </Card.Body>
-              <Card.Footer content={<div>{rowData.publishedAt.slice(0, 10)}</div>} />
-            </Card>
+        rowData.type !== '福利' ? (
+          <Card full onClick={() => {window.open(rowData.url)}}>
+            <Card.Body>
+              <Flex align="start">
+                {
+                  rowData.images &&
+                    <Flex.Item style={{ flex: '0 0 auto', width: 100, textAlign: 'center' }}>
+                      <img
+                        style={{ maxWidth: 100, maxHeight: 100 }}
+                        src={`${rowData.images[0].replace(/^http:\/\//i, 'https://')}?imageView2/0/w/200/h/200`}
+                        onError={(e) => e.target.src='/static/icon/image.svg'}
+                      />
+                    </Flex.Item>
+                }
+                <Flex.Item>
+                  <div>{rowData.desc}</div>
+                </Flex.Item>
+              </Flex>
+            </Card.Body>
+            <Card.Footer
+              content={
+                <div>
+                  {rowData.who ? rowData.who : ''}
+                  {rowData.publishedAt.slice(0, 10)}
+                </div>
+              }
+              extra={<Tag small>{rowData.type}</Tag>}
+            />
+          </Card>
+        ) : (
+          <Card full>
+            <Card.Body>
+              <img
+                style={{ maxWidth: '100%' }}
+                src={`${rowData.url}?imageView2/2/w/750`}
+                onError={(e) => e.target.src='/static/icon/image.svg'}
+              />
+            </Card.Body>
+            <Card.Footer content={<div>{rowData.publishedAt.slice(0, 10)}</div>} />
+          </Card>
+        )
       }
     </div>
   )
