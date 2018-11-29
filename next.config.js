6fc562a32c3f68a5b027701b8b624c1774be6237
OrangeXC <842012622@qq.com>
2018-11-29 10:07:57 +0800

    Update configs

--- next.config.js
+++ next.config.js
@@ -1,55 +1,9 @@
-const path = require('path')
-const fs = require('fs')
-const requireHacker = require('require-hacker')
 const withOffline = require('next-offline')
+const withCss = require('@zeit/next-css')
 
-function setupRequireHacker () {
-  const webjs = '.web.js'
-  const webModules = ['antd-mobile', 'rmc-picker'].map(m => path.join('node_modules', m))
-
-  requireHacker.hook('js', filename => {
-    if (filename.endsWith(webjs) || webModules.every(p => !filename.includes(p))) return
-
-    const webFilename = filename.replace(/\.js$/, webjs)
-    if (!fs.existsSync(webFilename)) return
-
-    return fs.readFileSync(webFilename, { encoding: 'utf8' })
-  })
-
-  requireHacker.hook('svg', filename => {
-    return requireHacker.to_javascript_module_source(`#${path.parse(filename).name}`)
-  })
+// fix: prevents error when .css files are required by node
+if (typeof require !== 'undefined') {
+  require.extensions['.css'] = (file) => {}
 }
 
-setupRequireHacker()
-
-function moduleDir (m) {
-  return path.dirname(require.resolve(`${m}/package.json`))
-}
-
-module.exports = withOffline({
-  webpack: (config) => {
-    config.resolve.extensions = ['.web.js', '.js', '.json']
-
-    config.module.rules.push({
-      test: /\.(svg)$/i,
-      loader: 'emit-file-loader',
-      options: {
-        name: 'dist/[path][name].[ext]'
-      },
-      include: [
-        moduleDir('antd-mobile'),
-        __dirname
-      ]
-    }, {
-      test: /\.(svg)$/i,
-      loader: 'svg-sprite-loader',
-      include: [
-        moduleDir('antd-mobile'),
-        __dirname
-      ]
-    })
-
-    return config
-  }
-})
+module.exports = withOffline(withCss())
