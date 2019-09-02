4fb8ad72107557d8c631efe6b78e0dac6c75b59a
OrangeXC <842012622@qq.com>
2019-09-02 16:19:56 +0800

    Add collectCoverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverage: true
 }
